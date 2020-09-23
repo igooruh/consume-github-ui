66dc3001e96afd768497dd610487cf12ae40a00e
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-23 19:13:04 -0300

    Changes name variable repository to fullName

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
 
 interface RepositoryParams {
-  repository: string;
+  fullName: string;
 }
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return <h1>Repository: {params.fullName}</h1>;
 };
 
 export default Repository;
